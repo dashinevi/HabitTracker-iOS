f2aa8d8e9051a184a601ec8b4f63d4d94154ee6a
Diana Dashynevich <ddashinevich@gmail.com>
2024-12-18 17:09:30 +0100

    feat: add home view with greeting and habit list

---
+++ HabitTracker/HomeView.swift
@@ -0,0 +1,55 @@
+//
+//  HomeView.swift
+//  HabitTracker
+//
+//  Created by Diana Dashinevich on 16/12/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var habits: [Habit]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("Hello, Kate!")
+                    .font(.title)
+                    .fontDesign(.rounded)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.pink)
+                
+                Text("How are you feeling today?")
+                    .font(.title3)
+                    .fontDesign(.rounded)
+                    .fontWeight(.bold)
+            }
+            .frame(width: 300, height: 80)
+            
+            HStack {
+                Spacer()
+                Text(Date.now, format: .dateTime.day().month().year())
+                    .font(.title3)
+                    .fontDesign(.rounded)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.secondary)
+            }
+            .padding()
+            
+            Spacer()
+            
+            HabitsListView(habits: $habits)
+        }
+    }
+}
+
+//#Preview {
+//    HomeView(
+//        habits: .constant([
+//            Habit(title: "title", description: "desc", color: .clear, goal: 10, frequency: 10, goalPeriod: "Week"),
+//            Habit(title: "title", description: "desc", color: .clear, goal: 10, frequency: 10, goalPeriod: "Week")
+//        ])
+//    )
+//}
