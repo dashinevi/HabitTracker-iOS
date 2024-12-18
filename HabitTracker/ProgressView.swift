b380db249d995f35efc0326013e6787d656f1f6c
Diana Dashynevich <ddashinevich@gmail.com>
2024-12-18 17:06:48 +0100

    feat: add progress view with habit statistics and current week display

---
+++ HabitTracker/ProgressView.swift
@@ -0,0 +1,42 @@
+//
+//  StatisticsView.swift
+//  HabitTracker
+//
+//  Created by Diana Dashinevich on 14/12/2024.
+//
+import SwiftUI
+
+struct ProgressView: View {
+    @Binding var habits: [Habit]
+    
+    
+    var body: some View {
+        VStack {
+            Text("Statistics")
+                .fontDesign(.rounded)
+                .font(.headline)
+                .fontWeight(.bold)
+            CurrentWeekView()
+        }
+        ScrollView {
+            VStack (spacing: 10) {
+                ForEach(habits) { habit in
+                    ProgressItemView(title: habit.title, description: habit.description, color: habit.color, goal: habit.goal, frequency: habit.frequency, goalPeriod: habit.goalPeriod, completedDates: habit.completedDates)
+                }
+            }
+            .padding()
+           
+        }
+    }
+}
+
+
+//#Preview {
+//    ProgressView(
+//        habits: .constant([
+//            Habit(title: "title", description: "desc", color: .clear, goal: 10, frequency: 10, goalPeriod: "Week"),
+//            Habit(title: "title", description: "desc", color: .clear, goal: 10, frequency: 10, goalPeriod: "Week")
+//        ])
+//    )
+//}
+
